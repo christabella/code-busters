239100a9e922d36240fc97ff26812e8b6f342401
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-04-08 09:06:14 +0800

    Finally passed "Go, Gopher!" by removing numpy usage (ugh runtime
    error was a module not found error probably)

--- gogopher.py
+++ gogopher.py
@@ -1,9 +1,10 @@
 import sys
 import math
-import numpy as np
+# import numpy as np
 
 def solve(width, row, col, dug):
-    current_square = dug[row-1:row+2, col-1:col+2]
+    # current_square = dug[row-1:row+2, col-1:col+2]
+    current_square = [dug[i][col-1:col+2] for i in range(row-1, row+2)]
     # If 3s3 square has been filled, move
     if sum([sum(x) for x in current_square]) == 9:
         col += 1  # move onto next column
@@ -15,10 +16,10 @@ def solve(width, row, col, dug):
     print(coords)
     sys.stdout.flush()
     x, y = [int(x) for x in input().split()]
-    if (x is y is 0) or (x is y is -1): 
+    if (x == y == 0) or (x == y == -1): 
         raise Exception("Gopher is ded")
-    dug[x, y] = 1
-    return row, col
+    dug[x][y] = 1
+    return row, col, dug
 
 cases = int(input())
 
@@ -32,11 +33,11 @@ for case in range(1, cases+1):
         else:
             width += 1
     row, col = 2, 2
-    dug = np.array([[0] * 1001] * 1001)
+    dug=[[0 for x in range(1001)] for y in range(1001)]
 
 
     while True:
         try:
-            row, col = solve(width, row, col, dug)
+            row, col, dug = solve(width, row, col, dug)
         except:
             break
