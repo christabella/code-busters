cb489283c41102d4359776b39f808d95f0255a58
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-11-14 15:44:07 +0200

    In-place recursive quicksort; Add random array tests

--- sorts/quicksort.py
+++ sorts/quicksort.py
@@ -1,25 +1,49 @@
+"""In-place sorting with O(log n) to O(n^2) time, and  O(log n) memory"""
+
 import unittest
+import random
 
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
-def quicksort(arr):
-    if len(arr) <= 1:
+def quicksort(arr, l=0, r=None):
+    """Recursively partition array's elements in-place w.r.t. a chosen pivot.
+
+    Put smaller (or equal) values to the left of pivot, and greater to the right.
+
+    Args:
+      arr: Array of numbers
+    """
+    if r is None: r = len(arr)
+    if l >= r-1:
         return
-    pivot = arr[-1]
-    pivot_pos = 0  # Where the pivot should go
-    for i in arr:
-        if arr[i] < pivot:
+    pivot = arr[r-1]  # Use last element as pivot
+    pivot_pos = l  # Where the pivot should go; intialize at the head
+    for i in range(l, r-1):  # The actual partitioning
+        if arr[i] <= pivot:
             swap(arr, i, pivot_pos)
-            print(arr)
             pivot_pos += 1  # Pivot is always on the right of smaller elements
-    quicksort(arr[:pivot])  # You dummy, doing this is not sorting it in place!
-    quicksort(arr[pivot+1:])
+    # Put the pivot where it should go
+    swap(arr, pivot_pos, r-1)
+    quicksort(arr, l=l, r=pivot_pos)
+    quicksort(arr, l=pivot_pos, r=r)
     return arr
 
+
 class Test(unittest.TestCase):
-    def test_s(self):
+    random.seed(10)
+    def test(self):
         arr = [0, 3, 1, 2]
+        actual = quicksort(arr)
+        expected = sorted(arr)
+        self.assertEqual(actual, expected)
+    def test_10(self):
+        arr = [random.randint(-100, 100) for i in range(10)]
+        actual = quicksort(arr)
+        expected = sorted(arr)
+        self.assertEqual(actual, expected)
+    def test_1000(self):
+        arr = [random.randint(-100, 100) for i in range(1000)]
         actual = quicksort(arr)
         expected = sorted(arr)
         self.assertEqual(actual, expected)
